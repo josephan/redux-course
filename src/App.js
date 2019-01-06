4364721a79488aa2c356cd483a0f8353bcce1b86
Joseph An <joseph@biorender.io>
2019-01-06 09:17:31 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm.js';
+import TodoList from './components/TodoList.js';
+import Message from './components/Message.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Todo-App">
+        <Message message='hello there' />
+        <TodoForm />
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {updateCurrent, saveTodo} from '../reducers/todo';
+
+class TodoForm extends Component {
+  handleInputChange = e => {
+    const value = e.target.value;
+    this.props.updateCurrent(value);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.saveTodo(this.props.currentTodo);
+  };
+
+  render() {
+    const {currentTodo} = this.props;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={currentTodo}
+        />
+      </form>
+    );
+  }
+}
+export default connect(
+  state => ({currentTodo: state.currentTodo}),
+  {updateCurrent, saveTodo},
+)(TodoForm);
